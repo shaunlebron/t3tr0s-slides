3ba5dee252817082ae27abcb5872ddc773d8f221
Shaun Williams <shaun.e.williams@gmail.com>
2016-08-03 22:53:16 -0500

    port slide 5

--- src/t3tr0s_slides/core.cljs
+++ src/t3tr0s_slides/core.cljs
@@ -5,6 +5,7 @@
     [t3tr0s-slides.slide02 :as slide02]
     [t3tr0s-slides.slide03 :as slide03]
     [t3tr0s-slides.slide04 :as slide04]
+    [t3tr0s-slides.slide05 :as slide05]
     [t3tr0s-slides.slide-wip :as slide-wip]))
 
 (enable-console-print!)
@@ -15,6 +16,7 @@
    {:id "slide02" :init slide02/init :resume slide02/resume :stop slide02/stop}
    {:id "slide03" :init slide03/init :resume slide03/resume :stop slide03/stop}
    {:id "slide04" :init slide04/init :resume slide04/resume :stop slide04/stop}
+   {:id "slide05" :init slide05/init :resume slide05/resume :stop slide05/stop}
    {:id "slide-wip" :init slide-wip/init :resume slide-wip/resume :stop slide-wip/stop}])
 
 (def current-slide (atom nil))
